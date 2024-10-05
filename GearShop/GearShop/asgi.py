8294b53d80d94ddac665f91ad2866241085fdd5f
Gia Kiet <giakiet241204@gmail.com>
2024-10-05 07:56:54 +0700

    Initial commit - Django project setup

---
+++ GearShop/GearShop/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for GearShop project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GearShop.settings")
+
+application = get_asgi_application()
